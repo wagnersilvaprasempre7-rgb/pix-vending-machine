10e830b78bdde5dfc717ef7053df2bca477927b0
wagnersilvaprasempre7-rgb <wagnersilvaprasempre7@gmail.com>
2026-06-26 15:57:33 -0300

    Add webhookSecret to config.js

--- config.js
+++ config.js
@@ -5,6 +5,8 @@ const config = {
 
   mercadoPagoAccessToken: process.env.MP_ACCESS_TOKEN,
 
+  webhookSecret: process.env.MP_WEBHOOK_SECRET,
+
   baseUrl: process.env.BASE_URL || "https://pix-vending-machine.onrender.com"
 };
 
